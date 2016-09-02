0aecda8d4849c941632643f9f37a9b7a968d4064
Khandar William <khandar.william@gdplabs.id>
2016-09-02 19:23:04 +0700

    Implement Book Model

---
+++ Model.php
@@ -0,0 +1,30 @@
+<?php
+
+$min_need = array (
+  'soldier' => 227,
+  'archer' => 227,
+  'scout' => 150,
+  'thief' => 150,
+  'healer' => 200,
+  'warrior' => 150,
+  'sniper' => 120,
+  'sorcerer' => 200,
+  'knight' => 150,
+  'ninja' => 100,
+  'alchemist' => 150,
+  'temple knight' => 150,
+  'cannoneer' => 90,
+  'necromancer' => 120,
+  'nightmare' => 156,
+  'assassin' => 90,
+  'cleric' => 155,
+  'paladin' => 150,
+  'meister' => 85,
+  'summoner' => 120,
+  'holy knight' => 100,
+  'shadow knight' => 100,
+  'priest' => 100,
+  'royalguard' => 65,
+  'wizard' => 45,
+);
+echo 'Min set level: '.array_sum($min_need)."\n";
